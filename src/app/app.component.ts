ff21ac048afb10d0c4511da74612b13437759428
Mingus <sermingus@gmail.com>
2024-02-15 23:44:11 +0100

    15_02_2024_23_43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+/* /src/app/app.component.ts */
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'loginsupabase';
+
+export class AppComponent implements OnInit {
+  
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor ( private router: Router ){}
+
+  ngOnInit(): void {
+    this.router.navigate(['/login']);
+  }
 }
---
+++ src/app/enviroment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ihsemymmyyxidbupnpim.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imloc2VteW1teXl4aWRidXBucGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MzQ4NDIsImV4cCI6MjAyMTQxMDg0Mn0.RP9HiL6vqHdF9r-Y0jRoJEIS7fqMD-dCWfxgoRGniIY',
+};
